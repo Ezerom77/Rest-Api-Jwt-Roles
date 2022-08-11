254adce93f4e83de0ed198ee76dd5d87d41f81e5
Ezerom77 <fullstack@ezerom.com.ar>
2022-08-11 17:04:20 -0300

    Roles Added

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
 const router = Router();
 import * as userController from '../controllers/user.controller'
-import { authJwt } from '../middlewares'; 
+import { authJwt, verifySignup} from '../middlewares'; 
 
 router.post('/',[
 authJwt.verifyToken,
 authJwt.isAdmin,
+verifySignup.checkRoleExisted
 ],
 userController.createUser)
 export default router;
